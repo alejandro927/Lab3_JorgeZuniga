1ed9dea3db5b6be1135364ec381483e29c696d4d
JorgeZuniga <alejandrojorge927@gmail.com>
2021-02-05 13:28:12 -0600

    Hice la estrutura del programa

---
+++ mainLab3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char** argv) 
+{
+	bool res;
+	res=true;
+	while(res==true) 
+	{
+		int opcion;
+		cout<<"1) Ejercicio1"<<endl;
+		cout<<"2) Ejercicio2"<<endl;
+		cout<<"3) Ejercicio3"<<endl;
+		cout<<"4) Ejercicio4"<<endl;
+		cout<<"5) Salida"<<endl;
+		cout<<"Ingrese su opcion: ";
+		cin>>opcion;
+		//imgresa la opcion que desea
+		switch(opcion) 
+		{
+			case 1:
+			{
+				
+				
+			}
+			break;
+			case 2:
+			{
+				
+				
+			}
+			break;
+			case 3:
+			{
+				
+				
+			}
+			break;
+				
+			case 4:
+			{
+				
+				
+			}	
+			break;
+			case 5:
+				res=false;
+				cout<<"Gracias por usar lel programa!!"<<endl;
+				break;
+		}
+	}
+}
