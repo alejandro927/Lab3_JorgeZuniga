1f949a537de43343d7a08a1d7520dcdb9c60262f
JorgeZuniga <alejandrojorge927@gmail.com>
2021-02-05 15:53:17 -0600

    termine el primero

--- mainLab3.cpp
+++ mainLab3.cpp
@@ -1,14 +1,101 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <string.h>
 using namespace std;
 
+void ejercicio1(string cadena) {
+	int size = cadena.length();
+	char chars[size];
+	cout<<"[";
 
-int main(int argc, char** argv) 
-{
+	for(int i = 0; i < size; i++) {
+
+		int auxiliar=0;
+		for(int j=0; j< size; j++) {
+
+			if(chars[j] == cadena[i]) {
+				auxiliar++;
+			}
+		}
+
+		if(auxiliar==0) {
+			chars[i] = cadena[i];
+			cout << chars[i] << " ";
+		}
+
+	}
+	cout<<"]";
+}
+
+vector<char> ejercicio11(string cadena) {
+	int size = cadena.length();
+	vector<char> chars;
+	//cout<<"[";
+	for(int i = 0; i < size; i++) {
+		chars.push_back(cadena[i]);
+	}
+	
+	for(int i=chars.size()-1; i>=0; i--) {
+		
+		int auxiliar=0;
+		for(int j = chars.size()-1; j >= 0 ; j--) {
+			if(chars.at(j) == cadena[i]) {
+				auxiliar++;
+			}
+		}
+		if(auxiliar != 1) {
+			/*char aux = cadena[i];
+			segundo.push_back(aux);
+			cout << chars.at(i) << " ";
+			*/
+			chars.erase(chars.begin()+i);
+		}
+
+	}
+	return chars;
+}
+
+void ejercicio2(string cadena) {
+	int size = cadena.length();
+	char chars[size];
+	cout<<"[";
+	for(int i = 0; i < size; i++) {
+
+		int auxiliar=0;
+		for(int j=0; j<size; j++) {
+
+			if(chars[j] == cadena[i]) {
+				auxiliar++;
+			}
+		}
+		if(auxiliar==0) {
+			chars[i] = cadena[i];
+			cout << chars[i] << " ";
+		}
+
+	}
+	cout<<"] = ";
+	cout<<"[ ";
+
+
+	for(int i = 0; i < size; i++) {
+		int auxiliar=0;
+		for(int j=0; j < size; j++) {
+			if(cadena[j] == chars[i]) {
+				auxiliar++;
+			}
+		}
+		cout<<" "<<auxiliar<<", ";
+
+	}
+	cout<<"]";
+}
+
+
+int main(int argc, char** argv) {
 	bool res;
 	res=true;
-	while(res==true) 
-	{
+	while(res==true) {
 		int opcion;
 		cout<<"1) Ejercicio1"<<endl;
 		cout<<"2) Ejercicio2"<<endl;
@@ -18,32 +105,42 @@ int main(int argc, char** argv)
 		cout<<"Ingrese su opcion: ";
 		cin>>opcion;
 		//imgresa la opcion que desea
-		switch(opcion) 
-		{
-			case 1:
-			{
-				
-				
+		switch(opcion) {
+			case 1: {
+				cout<<"Ingrese una cadena:";
+				string cadena = "";
+				getline(cin, cadena);
+				getline(cin, cadena);
+				cout<<endl;
+				//cout<<"La cadena es:"<<palabra<<endl;
+				vector<char> c = ejercicio11(cadena);
+				for(int i = 0 ; i < c.size() ; i++){
+					cout<< c.at(i)<<", ";
+				}
+				cout<<endl<<endl;
 			}
 			break;
-			case 2:
-			{
-				
-				
+			case 2: {
+				cout<<"Ingrese una cadena:";
+				string cadena = "";
+				getline(cin, cadena);
+				getline(cin, cadena);
+				cout<<endl;
+				ejercicio2(cadena);
+				cout<<endl<<endl;
 			}
 			break;
-			case 3:
-			{
-				
-				
+			case 3: {
+				string palabra="hola";
+
+
 			}
 			break;
-				
-			case 4:
-			{
-				
-				
-			}	
+
+			case 4: {
+
+
+			}
 			break;
 			case 5:
 				res=false;
